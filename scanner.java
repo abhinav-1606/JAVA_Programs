41c63e375a7ef682115b7c3baeeff6bc69e87912
abhinav-1606 <adutta1606@gmail.com>
2021-08-16 01:38:29 +0530

    commit

--- scanner.java
+++ scanner.java
@@ -4,9 +4,9 @@ public class scanner {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is the age of one?:-");
+        System.out.println("What is the age of one? :-");
         int ram = sc.nextInt();
-        System.out.println("What is the age of two?:-");
+        System.out.println("What is the age of two? :-");
         int shyam = sc.nextInt();
         int total = ram + shyam;
         System.out.println("Total Age: "+ total);
